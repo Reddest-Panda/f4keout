6cab18698b684736ea18f9c2bb10adaa0aeb0473
Skylar Gagnon <skylar_lab@undergrad.ad.unh.edu>
2025-03-19 08:31:39 -0400

    added test to see if aliasing effect occurs at cache line or word granularity

---
+++ experiments/granularity/contention.c
@@ -0,0 +1,313 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sched.h>
+#include <pthread.h>
+#include <sys/mman.h>
+
+//! ---- Variables ---- !//
+#define OFFSET 0x12F
+#define ITS 50000
+#define THREAD1_CPU 0
+#define THREAD2_CPU 8
+
+
+char *mem;
+char err_msg[] = "Invalid arguments, proper use: [victim setting] [attacker setting]\n\nVictim and Attacker Options:\nw - Writes\nr - Reads\nf - Flushes\n";
+char vic_setting;
+char att_setting;
+
+//! ---- ASM Functions ---- !//
+static inline __attribute__((always_inline)) void mfence() {
+	asm volatile("mfence\n");
+}
+
+static inline __attribute__((always_inline)) uint64_t rdtsc_begin() {
+        uint64_t a, d;
+        asm volatile(	
+            "mfence\n\t"
+            "CPUID\n\t"
+            "RDTSCP\n\t"
+            "mov %%rdx, %0\n\t"
+            "mov %%rax, %1\n\t"
+            "mfence\n"
+            : "=r" (d), "=r" (a)
+            :
+            : "%rax", "%rbx", "%rcx", "%rdx");
+        a = (d << 32) | a;
+        return a;
+}
+
+static inline __attribute__((always_inline)) uint64_t rdtsc_end() {
+        uint64_t a, d;
+        asm volatile(	
+            "mfence\n"
+            "RDTSCP\n\t"
+            "mov %%rdx, %0\n\t"
+            "mov %%rax, %1\n\t"
+            "CPUID\n\t"
+            "mfence\n"
+            : "=r" (d), "=r" (a)
+            :
+            : "%rax", "%rbx", "%rcx", "%rdx");
+        a = (d << 32) | a;
+        return a;
+}
+
+static inline __attribute__((always_inline)) void writes(char *ptr) {
+    asm volatile (
+        "mov %%al, 0x0000(%0)\n\t"
+        "mov %%al, 0x1000(%0)\n\t"	
+        "mov %%al, 0x2000(%0)\n\t"
+        "mov %%al, 0x3000(%0)\n\t"	
+        "mov %%al, 0x4000(%0)\n\t"
+        "mov %%al, 0x5000(%0)\n\t"	
+        "mov %%al, 0x6000(%0)\n\t"
+        "mov %%al, 0x7000(%0)\n\t"
+        :
+        : "rbx" (ptr)
+        : 
+    );
+}
+
+static inline __attribute__((always_inline)) void reads(char *ptr) {
+    asm volatile (
+        "mov 0x0000(%0), %%al\n\t"
+        "mov 0x1000(%0), %%al\n\t" 
+        "mov 0x2000(%0), %%al\n\t" 
+        "mov 0x3000(%0), %%al\n\t" 
+        "mov 0x4000(%0), %%al\n\t" 
+        "mov 0x5000(%0), %%al\n\t" 
+        "mov 0x6000(%0), %%al\n\t" 
+        "mov 0x7000(%0), %%al\n\t"
+        :
+        : "rbx" (ptr)
+        : 
+    );
+}
+
+static inline __attribute__((always_inline)) void mixed(char *ptr) {
+    asm volatile (
+        "mov 0x0000(%0), %%al\n\t" 
+        "mov %%al, 0x1000(%0)\n\t"
+        "mov 0x2000(%0), %%al\n\t" 
+        "mov %%al, 0x3000(%0)\n\t"	
+        "mov 0x4000(%0), %%al\n\t"
+        "mov %%al, 0x5000(%0)\n\t"	
+        "mov 0x6000(%0), %%al\n\t"
+        "mov %%al, 0x7000(%0)\n\t"
+        : 
+        : "rbx" (ptr)
+        :
+    );
+}
+
+static inline __attribute__((always_inline)) void flushes(char *ptr) {
+    asm volatile (
+        "clflush 0x0000(%0)\n\t"
+        "clflush 0x1000(%0)\n\t" 
+        "clflush 0x3000(%0)\n\t" 
+        "clflush 0x2000(%0)\n\t" 
+        "clflush 0x4000(%0)\n\t" 
+        "clflush 0x5000(%0)\n\t" 
+        "clflush 0x6000(%0)\n\t" 
+        "clflush 0x7000(%0)\n\t"
+        : 
+        : "rbx" (ptr)
+        :
+    );
+}
+
+static inline __attribute__((always_inline)) void single_write(char *ptr) {
+    asm volatile (
+        "mov %%cl, 0x0000(%0)\n\t"
+        // "mov %%al, 0x1000(%0)\n\t"
+        // "mov %%al, 0x2000(%0)\n\t"
+        // "mov %%al, 0x3000(%0)\n\t"
+        // "mov %%al, 0x4000(%0)\n\t"
+        // "mov %%al, 0x5000(%0)\n\t"
+        // "mov %%al, 0x6000(%0)\n\t"
+        // "mov %%al, 0x7000(%0)\n\t"
+        :
+        : "rbx" (ptr)
+        : 
+    );
+}
+
+static inline __attribute__((always_inline)) void single_read(char *ptr) {
+    asm volatile (
+        "mov 0x0000(%0), %%cl\n\t"
+        // "mov 0x1000(%0), %%al\n\t"
+        // "mov 0x2000(%0), %%al\n\t"
+        // "mov 0x3000(%0), %%al\n\t"
+        // "mov 0x4000(%0), %%al\n\t"
+        // "mov 0x5000(%0), %%al\n\t"
+        // "mov 0x6000(%0), %%al\n\t"
+        // "mov 0x7000(%0), %%al\n\t"
+        :
+        : "rbx" (ptr)
+        : 
+    );
+}
+
+static inline __attribute__((always_inline)) void single_mixed(char *ptr) {
+    asm volatile (
+        "mov 0x0000(%0), %%cl\n\t" 
+        "mov %%cl, 0x0000(%0)\n\t"
+        // "mov 0x2000(%0), %%al\n\t" 
+        // "mov %%al, 0x3000(%0)\n\t"
+        // "mov 0x4000(%0), %%al\n\t" 
+        // "mov %%al, 0x5000(%0)\n\t"
+        // "mov 0x6000(%0), %%al\n\t" 
+        // "mov %%al, 0x7000(%0)\n\t"
+        : 
+        : "rbx" (ptr)
+        :
+    );
+}
+
+static inline __attribute__((always_inline)) void single_flush(char *ptr) {
+    asm volatile (
+        "clflush 0x0000(%0)\n\t"
+        // "clflush 0x1000(%0)\n\t"
+        // "clflush 0x2000(%0)\n\t"
+        // "clflush 0x3000(%0)\n\t"
+        // "clflush 0x4000(%0)\n\t"
+        // "clflush 0x5000(%0)\n\t"
+        // "clflush 0x6000(%0)\n\t"
+        // "clflush 0x7000(%0)\n\t"
+        : 
+        : "r" (ptr)
+        :
+    );
+}
+
+//! ---- Utility Functions ---- !//
+void thread_setup(int core) {
+	cpu_set_t cpuset;
+	CPU_ZERO(&cpuset);
+	CPU_SET(core, &cpuset);
+	pthread_t thread;
+	thread = pthread_self();
+	int ret = pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
+	if (ret != 0) {
+		fprintf(stderr, "pthread_setaffinity_np() failed\n");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void* get_instruction(char setting) {
+	switch (setting) {
+		case 'w':
+			return writes;
+		case 'r':
+			return reads;
+		case 'f':
+			return flushes;
+		case 'm':
+			return mixed;
+		default: 
+			printf("%s", err_msg);
+			exit(EXIT_SUCCESS);
+	}
+}
+
+void* get_vic_instruction(char setting) {
+	switch (setting) {
+		case 'w':
+			return single_write;
+		case 'r':
+			return single_read;
+		case 'f':
+			return single_flush;
+		case 'm':
+			return single_mixed;
+		default: 
+			printf("%s", err_msg);
+			exit(EXIT_SUCCESS);
+	}
+}
+
+//! ---- Threads ---- !//
+void *attacker(void *args) {
+	// Setup
+	thread_setup(THREAD1_CPU); // Put on same physical core
+	void (*instruction)(void*) = get_instruction(att_setting); // Testing instruction passed by inline options
+
+	// Timing Instructions on the Same and Different Offsets to the Victim
+	uint64_t start, end;
+	unsigned char *ptr_diff = mem + 0xABCD; // Different address as victim
+	unsigned char *ptr_lower_match = mem + OFFSET + 0xA000; // Same lower 12 bits as victim
+	unsigned char *ptr_cl_match = mem + (OFFSET & (0xFF0)) + 0xA000; // Same lower 12 bits as victim
+
+	for (int i = 0; i < ITS; i++) {
+		start = rdtsc_begin();
+		instruction(ptr_diff);
+		end = rdtsc_end();
+		printf("%lu\n", end - start);
+	}
+
+	printf("|<END>|\n"); // Separater for data processing
+
+	for (int i = 0; i < ITS; i++) {
+		// Should be slow if theres contention
+		start = rdtsc_begin();
+		instruction(ptr_lower_match);
+		end = rdtsc_end();
+		printf("%lu\n", end - start);
+	}
+
+	printf("|<END>|\n"); // Separater for data processing
+
+	for (int i = 0; i < ITS; i++) {
+		// Should be slow if theres contention
+		start = rdtsc_begin();
+		instruction(ptr_cl_match);
+		end = rdtsc_end();
+		printf("%lu\n", end - start);
+	}
+}
+
+void *victim(void *args) {
+	thread_setup(THREAD2_CPU); // Put on same physical core
+	void (*instruction)(void*) = get_vic_instruction(vic_setting);  // Testing instruction passed by inline options
+
+	// Forever Running Instructions in Victim Thread
+	unsigned char *ptr = mem + OFFSET;	
+	mfence();
+	while(1) {
+		instruction(ptr);
+	}
+}
+
+//! ---- Main ---- !//
+int main(int argc, char **argv[]) {
+	// Parameter Processing //
+	if (argc != 3) {
+		printf("%s", err_msg);
+		return EXIT_SUCCESS;
+	} else {
+		vic_setting = (char)argv[1][0];
+		att_setting = (char)argv[2][0];
+	}
+
+
+	mem = (unsigned char *)mmap(NULL, 50 * 4096, 
+			PROT_READ | PROT_WRITE,
+			MAP_ANONYMOUS | MAP_PRIVATE | MAP_POPULATE, -1, 0);
+	memset(mem, 0x80, 50 * 4096);
+
+	// Launching Threads
+	pthread_t victim_thread, attacker_thread;
+	
+	pthread_create(&victim_thread, NULL, victim, NULL);
+	pthread_create(&attacker_thread, NULL, attacker, NULL);
+	
+	pthread_join(attacker_thread, NULL);
+	pthread_cancel(victim_thread);
+
+	return EXIT_SUCCESS;
+}
---
+++ experiments/granularity/run.py
@@ -0,0 +1,98 @@
+import os, itertools
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from datetime import datetime
+from tqdm import tqdm
+
+#! --- Small class for Measurements --- !#
+
+def read_data(filename):
+    data_readings = [[], [], []]
+    i = 0
+    with open(f"{filename}", "r") as f:
+        for line in f:
+            if line.strip() == "|<END>|":
+                i += 1
+                continue
+            data_readings[i].append(int(line))
+    return data_readings
+
+def remove_outliers(data, threshold=1.5):
+    """Removes outliers from the data using the IQR method."""
+    if len(data) == 0:
+        return data
+    q1 = np.percentile(data, 25)
+    q3 = np.percentile(data, 75)
+    iqr = q3 - q1
+    lower_bound = q1 - threshold * iqr
+    upper_bound = q3 + threshold * iqr
+    return [x for x in data if lower_bound <= x <= upper_bound]
+
+def process(readings):
+    return [stats.tmean(remove_outliers(addr)) for addr in readings]
+
+def parse_data(data):
+    options = ['f', 'r', 'w', 'm']
+    separated_data = []
+    for vic in options:
+        for att in options:
+            separated_data.append(data.loc[(data['vic'] == vic ) & (data['att'] == att)])
+    return separated_data
+
+def graph_all(data, name="prefetching"):
+    # Create a figure with a 4x4 grid of subplots
+    fig, axes = plt.subplots(4, 4, figsize=(15, 15))
+    separated_data = parse_data(data)
+    for idx, scenario_data in enumerate(separated_data):
+        ax = axes[idx // 4, idx % 4]
+        x_axis = [n for n in range(len(scenario_data['diff_avg']))]
+
+        ax.scatter(x_axis, scenario_data['diff_avg'].values, label='Diff', color='blue', linestyle='dotted')
+        ax.scatter(x_axis, scenario_data['lm_avg'], label='4k Aliasing', color='orange', linestyle='dotted')
+        ax.scatter(x_axis, scenario_data['cl_avg'], label='CL', color='green', linestyle='dotted')
+
+    # Add overarching row and column labels
+    row_labels = ["Victim Flush", "Victim Read", "Victim Write", "Victim Mixed"]
+    col_labels = ["Attacker Flush", "Attacker Read", "Attacker Write", "Attacker Mixed"]
+
+    for ax, col_label in zip(axes[0], col_labels):
+        ax.set_title(col_label, pad=30, fontsize=14)
+
+    for ax, row_label in zip(axes[:, 0], row_labels):
+        ax.set_ylabel(row_label, rotation="vertical", labelpad=40, fontsize=14, va='center')
+
+    # Adjust layout
+    plt.tight_layout(rect=[0.05, 0.05, 1, 0.95])
+    plt.savefig("data/plots/contention.png")
+
+
+#! --- Run Tests --- !#
+ITS = 100
+options = ['f', 'r', 'w', 'm']
+all_cases = [options, options]
+
+timestamp = datetime.now()
+timestamp_data = timestamp.strftime('%d') + '_' + timestamp.strftime('%m') + '_' + timestamp.strftime('%Y') + '_' + timestamp.strftime('%H:%M:%S')
+
+## Setup for data collection
+os.makedirs('data/tmp', exist_ok=True)
+os.makedirs('data/plots', exist_ok=True)
+os.makedirs('data/csvs', exist_ok=True)
+os.system("gcc -lpthread -w -O0 -o test contention.c")
+
+## Collect Data
+data = pd.DataFrame(columns=["vic", "att", "diff_avg", "lm_avg", "cl_avg"])
+for _ in tqdm(range(ITS)):
+    for vic, att in itertools.product(*all_cases): # Full sweep loop
+        os.system(f"./test {vic} {att} > data/tmp/{vic}-{att}")
+        readings = read_data(f'data/tmp/{vic}-{att}')
+        avgs = process(readings)
+        data = pd.concat([data, pd.DataFrame([[vic, att, avgs[0], avgs[1], avgs[2]]], columns=data.columns)], ignore_index=True) # appending row of data
+
+os.system("rm test")
+# ## Saving / Graphing
+data.to_csv(f"data/csvs/{timestamp_data}.csv")
+# data = pd.read_csv("data/all_same_core.csv")
+graph_all(data)
